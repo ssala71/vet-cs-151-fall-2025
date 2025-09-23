36e9b492deddb15eea4c15d80f260e40b69e6b54
liemmma <lic29000@gmail.com>
2025-09-22 21:51:35 -0700

    add attr. and constructor to owner and pet

--- src/Owner.java
+++ src/Owner.java
@@ -1,5 +1,19 @@
 public class Owner {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    // Attributes
+    private String name;
+    private String phoneNumber;
+    private String email;
+    private String address;
+    private int age;
+    private String gender;
+
+    // Constructor
+    public Owner(String name, String phoneNumber, String email, String address, int age, String gender){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+        this.age = age;
+        this.gender = gender;   
     }
 }
